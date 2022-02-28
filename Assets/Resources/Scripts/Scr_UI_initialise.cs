2e207cba0b627053c173c1fa4aa8bc252dd815b2
Woolford-Totton <97439457+Woolford-Totton@users.noreply.github.com>
2022-02-28 11:30:31 +0000

    Made Editor Version

--- Assets/Resources/Scripts/Scr_UI_initialise.cs
+++ Assets/Resources/Scripts/Scr_UI_initialise.cs
@@ -8,15 +8,14 @@ using System.IO;
 
 public class Scr_UI_initialise : MonoBehaviour
 {
-    private List<GameObject> UIButtons;
+    public GameObject List;
 
-    private List<string> UI_List;
+    private List<string> UI_List = new List<string>();
 
-    private int UIButtons_Count;
 
-    private int UIList_Count;
+    private int UIList_Count=0;
 
-    private int UIList_MaxCount;
+    public int UIList_MaxCount=10;
 
     private string FileName = "YourList.sav";
 
@@ -24,144 +23,11 @@ public class Scr_UI_initialise : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        UI_List = new List<string>();
-        UIList_Count = 0;
-        UIList_MaxCount = 10;
-
-        #region Initialise UI layout
-
-        UIButtons = new List<GameObject>();
-        UIButtons_Count = 0;
-
-        // UIButtons[0] = new GameObject ();
-        var prefabbutton = Resources.Load("Prefab/Btn_Empty");
-        
-        UIButtons.Add((GameObject)Instantiate(prefabbutton
-            , new Vector3(0, 0, 0), Quaternion.identity));
-
-        UIButtons[UIButtons_Count].transform.SetParent(transform);
-
-        Button btn_save_btn = UIButtons[UIButtons_Count].GetComponent<Button>();
-
-        Text btn_save_btn_text = btn_save_btn.GetComponentInChildren<Text>();
-
-        btn_save_btn_text.text = "Save";
-
-        RectTransform btn_save_transform = UIButtons[UIButtons_Count].GetComponent<RectTransform>();
-
-        UIButtons[UIButtons_Count].transform.Translate(btn_save_transform.rect.width * 0.5f
-            , (Screen.height * 1.0f) - btn_save_transform.rect.height * 0.5f, 0);
-
-        btn_save_btn.onClick.AddListener(Savegame);
-
-        UIButtons_Count += 1;
-
-        prefabbutton = Resources.Load("Prefab/Btn_Empty");
-
-        UIButtons.Add((GameObject)Instantiate(prefabbutton
-            , new Vector3(0, 0, 0), Quaternion.identity));
-
-        UIButtons[UIButtons_Count].transform.SetParent(transform);
-
-        btn_save_btn = UIButtons[UIButtons_Count].GetComponent<Button>();
-
-        btn_save_btn_text = btn_save_btn.GetComponentInChildren<Text>();
-
-        btn_save_btn_text.text = "Add Item";
-
-        btn_save_transform = UIButtons[UIButtons_Count].GetComponent<RectTransform>();
-
-
-
-        UIButtons[UIButtons_Count].transform.position= new Vector3( UIButtons[UIButtons_Count-1].transform.position.x,
-            UIButtons[UIButtons_Count-1].transform.position.y - btn_save_transform.rect.height * 1.0f, 0.0f);
-
-        
-        btn_save_btn.onClick.AddListener(AddItem);
-
-        UIButtons_Count += 1;
-
-        prefabbutton = Resources.Load("Prefab/Btn_Empty");
-
-        UIButtons.Add((GameObject)Instantiate(prefabbutton
-            , new Vector3(0, 0, 0), Quaternion.identity));
-
-        UIButtons[UIButtons_Count].transform.SetParent(transform);
-
-        btn_save_btn = UIButtons[UIButtons_Count].GetComponent<Button>();
-
-        btn_save_btn_text = btn_save_btn.GetComponentInChildren<Text>();
-
-        btn_save_btn_text.text = "Subtract Item";
-
-        btn_save_transform = UIButtons[UIButtons_Count].GetComponent<RectTransform>();
-
-
-
-        UIButtons[UIButtons_Count].transform.position = new Vector3(UIButtons[UIButtons_Count - 1].transform.position.x,
-            UIButtons[UIButtons_Count - 1].transform.position.y - btn_save_transform.rect.height * 1.0f, 0.0f);
-
-
-        btn_save_btn.onClick.AddListener(SubtractItem);
-
-        UIButtons_Count += 1;
-
-        prefabbutton = Resources.Load("Prefab/Label_Empty");
-
-        UIButtons.Add((GameObject)Instantiate(prefabbutton
-            , new Vector3(0, 0, 0), Quaternion.identity));
-
-        UIButtons[UIButtons_Count].transform.SetParent(transform);
-
-        btn_save_btn_text = UIButtons[UIButtons_Count].GetComponent<Text>();
-
-        btn_save_btn_text.verticalOverflow = VerticalWrapMode.Overflow;
-
-        var list_text = "My Saved List:";
-
+    {  
         if (System.IO.File.Exists(FilePath+FileName))
         {
-            var currentitem = "";
-
-            StreamReader Savedata = new StreamReader(FilePath + FileName);
-
-            currentitem = Savedata.ReadLine();
-
-            while (Savedata.EndOfStream == false)
-            {
-                currentitem = Savedata.ReadLine();
-
-                list_text +="\n" + currentitem;
-
-
-                UI_List.Add(currentitem);
-
-                UIList_Count += 1;
-            }
-
-
-
-
-            
-
-            Savedata.Close();
+            LoadList();
         }
-
-        btn_save_btn_text.text = list_text;
-
-        btn_save_transform = UIButtons[UIButtons_Count].GetComponent<RectTransform>();
-
-        UIButtons[UIButtons_Count].transform.Translate((Screen.width * 1.0f) - btn_save_transform.rect.width * 0.5f
-            , (Screen.height * 1.0f) - btn_save_transform.rect.height * 0.5f, 0);
-
-        UIButtons_Count += 1;
-
-        #endregion
-
-       
-
-
     }
 
     // Update is called once per frame
@@ -170,10 +36,35 @@ public class Scr_UI_initialise : MonoBehaviour
         
     
     }
+    void LoadList()
+    {
+        var list_text = "My Saved List:";
 
+        var currentitem = "";
+
+        StreamReader Savedata = new StreamReader(FilePath + FileName);
+
+        currentitem = Savedata.ReadLine();
+
+        while (Savedata.EndOfStream == false)
+        {
+            currentitem = Savedata.ReadLine();
+
+            list_text += "\n" + currentitem;
+
+
+            UI_List.Add(currentitem);
+
+            UIList_Count += 1;
+        }
+
+        Savedata.Close();
+
+        UpdateList();
+    }
     void UpdateList()
     {
-        var btn_save_btn_text = UIButtons[UIButtons_Count - 1].GetComponent<Text>();
+        var btn_save_btn_text = List.GetComponent<Text>();
 
         var list_text = "My Saved List:";
 
@@ -185,7 +76,7 @@ public class Scr_UI_initialise : MonoBehaviour
         btn_save_btn_text.text = list_text;
     }
 
-    void SubtractItem()
+    public void SubtractItem()
     {
         if (UIList_Count > 0)
         {
@@ -196,7 +87,7 @@ public class Scr_UI_initialise : MonoBehaviour
             UpdateList();
         }
     }
-    void AddItem()
+    public void AddItem()
     {
         if (UIList_Count < UIList_MaxCount)
         {
@@ -208,10 +99,8 @@ public class Scr_UI_initialise : MonoBehaviour
             UpdateList();
 
         }
-
-
     }
-    void Savegame()
+    public void Savegame()
     {
         var list_text = "My Saved List:";
 
